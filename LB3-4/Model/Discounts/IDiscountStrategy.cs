e0a66e10d6aa1bb9fb51e068edc0d2017291ce7f
StarovoytovDE <starovoytovde@gmail.com>
2025-12-11 07:43:28 +0700

    Translate discount comments to Russian

---
+++ LB3-4/Model/Discounts/IDiscountStrategy.cs
@@ -0,0 +1,26 @@
+namespace Model.Discounts;
+
+/// <summary>
+/// Представляет абстракцию для расчёта скидок на покупку.
+/// </summary>
+public interface IDiscountStrategy
+{
+    /// <summary>
+    /// Человекопонятное описание типа скидки.
+    /// </summary>
+    string Description { get; }
+
+    /// <summary>
+    /// Вычисляет сумму скидки для указанной суммы покупки.
+    /// </summary>
+    /// <param name="purchaseAmount">Положительная сумма покупки.</param>
+    /// <returns>Значение скидки, которое может быть применено.</returns>
+    decimal CalculateDiscount(decimal purchaseAmount);
+
+    /// <summary>
+    /// Вычисляет итоговую стоимость после применения скидки.
+    /// </summary>
+    /// <param name="purchaseAmount">Положительная сумма покупки.</param>
+    /// <returns>Итоговая сумма после скидки.</returns>
+    decimal CalculatePrice(decimal purchaseAmount);
+}
