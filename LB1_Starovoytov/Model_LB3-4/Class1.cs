2c082e815d0540bebefbbd67f689117298ce5a91
Dmitrii <Dmitrii@DMITRII>
2025-12-11 07:25:44 +0700

    Подготовка к ЛБ3

---
+++ LB1_Starovoytov/Model_LB3-4/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_LB3_4
+{
+    public class Class1
+    {
+    }
+}
---
+++ LB1_Starovoytov/Program_LB3-4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_LB3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
