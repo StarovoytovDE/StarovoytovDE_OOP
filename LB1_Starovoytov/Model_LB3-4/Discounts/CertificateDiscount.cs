27f826c359e05703d103f31c625d00f604e11283
Dmitrii <Dmitrii@DMITRII>
2025-12-11 08:01:23 +0700

    features(LB3_Starovoytov): выполнены пункты 1-3

---
+++ LB1_Starovoytov/Model_LB3-4/Discounts/CertificateDiscount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_LB3_4.Discounts
+{
+    /// <summary>
+    /// Использует фиксированное значение сертификата для уменьшения цены покупки.
+    /// </summary>
+    public sealed class CertificateDiscount : IDiscountStrategy
+    {
+        private decimal _certificateValue;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="CertificateDiscount"/>.
+        /// </summary>
+        /// <param name="certificateValue">Номинал сертификата.</param>
+        public CertificateDiscount(decimal certificateValue)
+        {
+            CertificateValue = certificateValue;
+        }
+
+        /// <summary>
+        /// Денежное значение сертификата. Должно быть положительным.
+        /// </summary>
+        public decimal CertificateValue
+        {
+            get => _certificateValue;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Номинал сертификата должен быть положительным.");
+                }
+
+                _certificateValue = value;
+            }
+        }
+
+        /// <inheritdoc />
+        public string Description => $"Скидка по сертификату на сумму {CertificateValue:C}";
+
+        /// <inheritdoc />
+        public decimal CalculateDiscount(decimal purchaseAmount)
+        {
+            ValidatePurchaseAmount(purchaseAmount);
+            return Math.Min(purchaseAmount, CertificateValue);
+        }
+
+        /// <inheritdoc />
+        public decimal CalculatePrice(decimal purchaseAmount)
+        {
+            ValidatePurchaseAmount(purchaseAmount);
+            return purchaseAmount - CalculateDiscount(purchaseAmount);
+        }
+
+        private static void ValidatePurchaseAmount(decimal purchaseAmount)
+        {
+            if (purchaseAmount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(purchaseAmount), "Сумма покупки должна быть положительной.");
+            }
+        }
+    }
+}
---
+++ LB1_Starovoytov/Model_LB3-4/Discounts/PercentageDiscount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_LB3_4.Discounts
+{
+    /// <summary>
+    /// Рассчитывает скидку как процент от суммы покупки.
+    /// </summary>
+    public sealed class PercentageDiscount : IDiscountStrategy
+    {
+        private decimal _percentage;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="PercentageDiscount"/>.
+        /// </summary>
+        /// <param name="percentage">Процент скидки в диапазоне от 0 до 100.</param>
+        public PercentageDiscount(decimal percentage)
+        {
+            Percentage = percentage;
+        }
+
+        /// <summary>
+        /// Процент скидки. Значение должно быть в диапазоне от 0 до 100 включительно.
+        /// </summary>
+        public decimal Percentage
+        {
+            get => _percentage;
+            set
+            {
+                if (value is < 0 or > 100)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), "Процент должен быть между 0 и 100.");
+                }
+
+                _percentage = value;
+            }
+        }
+
+        /// <inheritdoc />
+        public string Description => $"Процентная скидка {Percentage}%";
+
+        /// <inheritdoc />
+        public decimal CalculateDiscount(decimal purchaseAmount)
+        {
+            ValidatePurchaseAmount(purchaseAmount);
+            return decimal.Round(purchaseAmount * Percentage / 100m, 2, MidpointRounding.AwayFromZero);
+        }
+
+        /// <inheritdoc />
+        public decimal CalculatePrice(decimal purchaseAmount)
+        {
+            ValidatePurchaseAmount(purchaseAmount);
+            return decimal.Round(purchaseAmount - CalculateDiscount(purchaseAmount), 2, MidpointRounding.AwayFromZero);
+        }
+
+        private static void ValidatePurchaseAmount(decimal purchaseAmount)
+        {
+            if (purchaseAmount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(purchaseAmount), "Сумма покупки должна быть положительной.");
+            }
+        }
+    }
+}
